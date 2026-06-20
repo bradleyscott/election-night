6d0c9214722b071fb6096ac72395f16d1055354b
Bradley Scott <bradleyscott@users.noreply.github.com>
2026-06-20 12:30:49 +1200

    Change Te Pati Maori and NZ First colours

--- packages/dashboard/src/lib/constants.ts
+++ packages/dashboard/src/lib/constants.ts
@@ -3,7 +3,7 @@ export const partyColors: Record<string, string> = {
   'Labour Party': '#D82A20',
   'Green Party': '#098137',
   'ACT New Zealand': '#FFD700',
-  'New Zealand First Party': '#000000',
-  'Te Pāti Māori': '#A52A2A',
+  'New Zealand First Party': '#333333',
+  'Te Pāti Māori': '#000000',
   'The Opportunities Party (TOP)': '#4B0082',
 };
